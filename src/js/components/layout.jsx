9bfeaac2147617b5164b7dfbfbf5ab9236fec5fe
Jonathan <jonnywrites@gmail.com>
2017-12-05 01:25:25 +0000

    fixed some errors

--- src/js/components/layout.jsx
+++ src/js/components/layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
 import { Error404 } from './error404.jsx';
 import { Dashboard } from './dashboard.jsx';
 import { Home } from './home.jsx';
@@ -11,17 +10,14 @@ export class Layout extends React.Component{
     render(){
         return(
             <BrowserRouter>
-                    <div className="container-fluid no-gutters px-0">
-                        <Switch>
-                            <Route exact path='/' component={Home} />
-                            <Route exact path='/dashboard' component={Dashboard} />
-                            <Route exact path='/login' component={Login} />
-                            <Route render={Error404} />
-                        </Switch>
-                    </div>
-                </BrowserRouter>
-                
-            
+                <div className="container-fluid no-gutters px-0">
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/dashboard' component={Dashboard} />
+                        <Route render={Error404} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
             );
     }
     
