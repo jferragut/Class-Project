a8afab49bed6c73ca9206a0e7a70efb1321fc4d2
Gabriel Innecco <innecco9@gmail.com>
2017-12-09 02:19:51 +0000

    styled home components

--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -33,26 +33,26 @@ export class Home extends React.Component{
               <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
             <div className="carousel-inner">
-              <div className="carousel-item active">
+              <div className="carousel-item carousel-fade active">
                 <img className="d-block w-100 im" src={ 'public/'+carousel1 } alt="First slide"/>
                   <div className="carousel-caption">
-                    <h1 className="h1-responsive">Title 1</h1>
-                    <p>First text</p>
+                    <h1 className="h1-responsive">THE NEXT BIG THING IS HERE </h1>
+                    <p>Physical currency is old news</p>
                   </div>
               </div>
-              <div className="carousel-item">
+              <div className="carousel-item carousel-fade">
                 <img className="d-block w-100 im" src={ 'public/'+carousel2 } alt="Second slide"/>
               <div className="carousel-caption">
-                <h1 className="h1-responsive">Title 1</h1>
-                  <p>Second text</p>
+                <h1 className="h1-responsive">THE FUTURE IS NOW</h1>
+                  <p>DONT WAIT</p>
               </div>
               </div>
               
-              <div className="carousel-item">
+              <div className="carousel-item carousel-fade">
                 <img className="d-block w-100 im" src={ 'public/'+carousel3 } alt="Third slide"/>
               <div className="carousel-caption">
-                <h1 className="h1-responsive">Title 1</h1>
-                <p>Third text</p>
+                <h1 className="h1-responsive">INVESTING TIPS ON THE GO</h1>
+                <p>ON YO SMARTPHONE</p>
               </div>
             </div>
             
@@ -71,18 +71,18 @@ export class Home extends React.Component{
         
          <div className="jumbotron jumbotron-fluid" >
                 <div className="container">
-                    <h1 className="display-3">What is Crypto Currency?</h1>
-                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    <h1 className="display-3 jt">What is Crypto Currency?</h1>
+                    <hr className="my-4" />
+                    <p className="lead jst">Cryptocurrencies, sometimes called virtual currencies, digital money/cash, or tokens, are not really like U.S. dollars or British pounds. They live online and are not backed by a government. They’re backed by their respective networks. Technically speaking, cryptocurrencies are restricted entries in a database. Specific conditions must be met to change these entries. Created with cryptography, the entries are secured with math, not people.</p>
                 </div>
             </div>
             
             <div className="jumbotron-fluid">
-                <h1 className="display-3">What We Do</h1>
-                <p className="lead">nothing yet</p>
+                <h1 className="display-3 jt">What We Do</h1>
+                <p className="lead jst">nothing yet</p>
                 <hr className="my-4" />
-                <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
                 <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    <a className="btn btn-success btn-lg" href="#" role="button">Join Today</a>
                 </p>
             </div>
       
